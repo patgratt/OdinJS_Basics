5eab6731114f4ac28984ed3b023243063c4a7694
33patburke <33patburke@gmail.com>
2022-06-10 00:12:40 -0400

    finished fundamentals part 1

---
+++ fundametalsPart1/part1.js
@@ -0,0 +1,25 @@
+// Practice
+
+// 1
+console.log(23 + 97)
+
+// 2
+console.log(23 + 11 + 55 + 44 + 00 + 84)
+
+// 3
+console.log((4 + 6 + 9) / 77)
+
+// 4 
+let a = 10
+console.log(a)
+9 * a
+let b = 7 * a
+console.log(b)
+
+// 5 - const is different in that it declares a variable that cannot be reassigned, 
+//     doing so will result in an error
+const max = 57
+let actual = max - 13
+let percentage = actual / max
+console.log(percentage)
+
