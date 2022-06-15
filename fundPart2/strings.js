c7bbb9105c94e14f758208267b18639a092ee9c7
33patburke <33patburke@gmail.com>
2022-06-15 10:19:11 -0400

    meant to push all this too

--- fundPart2/strings.js
+++
@@ -1,9 +0,0 @@
-// concatenating strings in JS
-// use backtick mark (`) this is called a template literal
-
-const name = 'Chris';
-const greeting = `Hello, ${name}`;
-console.log(greeting); // Hello, Chris
-
-
-
